52d28d378852b727f73534dd9e017bf971ed95d3
Daniel Fleischer <danflscr@gmail.com>
2024-04-20 18:55:48 +0300

    Macos fix

--- alert.el
+++ alert.el
@@ -869,7 +869,7 @@ From https://github.com/julienXX/terminal-notifier."
   (if alert-notifier-command
       (let ((args
              (list "-title"   (alert-encode-string (plist-get info :title))
-                   "-appIcon" (or (plist-get info :icon) alert-notifier-default-icon)
+                   "-contentImage" (or (plist-get info :icon) alert-notifier-default-icon)
                    "-message" (alert-encode-string (plist-get info :message)))))
         (apply #'call-process alert-notifier-command nil nil nil args))
     (alert-message-notify info)))
